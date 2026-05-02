9db0bb8e1aa0bc86649b358142aa0ffddc1d7ac7
MohammadHosseinMoradi <mohammadhosein.zmoradi@gmail.com>
2026-05-02 15:58:16 +0330

    fix:get my exersice

--- src/routes/portal.route.ts
+++ src/routes/portal.route.ts
@@ -1,18 +1,43 @@
 import express from "express";
-
+import axios from "axios";
 const router = express.Router();
 
 router.get("/get-my-exercises", (_req, res) => {
   res.render("register", { title: "Register", error: null });
 });
 
-router.post("/get-my-exercises", (_req, res) => {
-  // proxy get-my-exercise
+router.post("/get-my-exercises", async(_req, res) => {
+const response = await axios.get(
+  process.env.API_GET_MY_EXERCISES,
+  {
+    params: {
+      limit: _req.query.limit,
+      offset: _req.query.offset,
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+    },
+  }
+);
+
+return res.json(response.data);
 });
 
 router.get("/execute/:key", (_req, res) => {
-  // redirect to execute
-  // res.redirect(...)
+    const url = new URL(_req.url);
+
+    // example: /exercise?key=abc123
+    const exerciseKey = url.searchParams.get("key");
+
+    if (!exerciseKey) {
+      return new Response("Missing exerciseKey", { status: 400 });
+    }
+
+    const baseUrl = process.env.API_REDIRECT_EXECUTE_EXERCISE!;
+
+    const targetUrl = baseUrl.replace("{exerciseKey}", exerciseKey);
+
+    return Response.redirect(targetUrl, 307); // keep method if POST
 });
 
 router.get("/create/:key", (_req, res) => {
